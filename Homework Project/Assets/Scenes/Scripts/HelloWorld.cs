4ab95ece7350237a8f494715d31bb9a4389e6d70
Stealth245 <dippo245@gmail.com>
2024-08-29 22:28:03 -0400

    Update HelloWorld.cs

--- Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
@@ -8,6 +8,14 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, world!");
+        Debug.LogWarning("Do NOT look down!");
+        Debug.LogError("Error! Error! rorrE!");
+        //Author: Jonathan Munro
+        /*
+         * I hope to learn how to use these programs to benifit my players in DND to give them a more immersive experience.
+         * Does seening how something looks change the narative?
+         * I hope to learn more about setting up a world and developing the aspects around the world.
+        */
     }
 
     // Update is called once per frame
