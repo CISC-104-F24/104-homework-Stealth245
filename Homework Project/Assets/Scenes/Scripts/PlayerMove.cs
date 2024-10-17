1a7796192e7052c26f45090767f3b23b923dfffc
Stealth245 <dippo245@gmail.com>
2024-10-16 21:49:51 -0400

    Confusion Update
    Don't think this works :(

--- Homework Project/Assets/Scenes/Scripts/PlayerMove.cs
+++ Homework Project/Assets/Scenes/Scripts/PlayerMove.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float moveSpeed;
+    //movement
+    private float moveSpeed;
     public float dirX, dirZ;
+    public float sprintSpeed;
+    public float walkSpeed;
+    public float currentSpeed;
+
+    [SerializeField] private Movement movement;
+
+    //other than movement
+    public Rigidbody rb;
+
+    //keybinds
+    public KeyCode SprintKey = KeyCode.LeftShift;
+
+    //other pt2
+    private float movementState; public System.Single GetMovementState()
+    {
+        return movementState;
+    }
+
+    public void SetMovementState(System.Single value)
+    {
+        movementState = value;
+    }
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air,
+    }
+private void Update()
+{
+    var targetAngle = Mathf.Atan2(dirX, dirZ) = Mathf.Rad2Deg;
+    transform.rotation = Quaterion.Euler(0.0f, targetAngle, 0.0f);
+}
+public struct Movement
+{
+    public bool isSprinting;
+}
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +53,7 @@ public class PlayerMove : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+
     // Update is called once per frame
     void Update()
     {
@@ -22,13 +61,21 @@ public class PlayerMove : MonoBehaviour
         dirZ = Input.GetAxis("Vertical") * moveSpeed;
 
 
-        if(Input.GetButtonDown("Jump")) {
+        if (Input.GetKeyDown("Jump")) {
          rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
         }
+
+        public void Sprint(InputAction.CallbackContext context)
+        {
+            movement.isSprinting = context.started || context.p
+        }
+
+        moveSpeed = new Vector3 (moveHorizontal, 0.0f, moveVertical);
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+        rb.AddForce (walkSpeed * currentSpeed);
     }
 }
